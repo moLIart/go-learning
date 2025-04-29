d2114a286c56360646b1a46932c0d22610424569
Anton Petrov <irbisapp54@gmail.com>
2025-04-29 20:56:46 +0300

    8th-hw + implement graceful shutdown for goroutines using context

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,25 @@
 package main
 
 import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/moLIart/go-course/internal/service"
 )
 
 func main() {
-	service.StartProcessing(19 * time.Millisecond)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-signalChan
+		cancel()
+	}()
+
+	service.StartProcessing(ctx, 19*time.Millisecond)
 }
