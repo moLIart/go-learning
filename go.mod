1f6d18eecfbb31dc10c3e93da8b0480518b43362
Anton Petrov <irbisapp54@gmail.com>
2025-05-28 00:01:23 +0300

    12th-hw + added files

--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/swaggo/http-swagger v1.3.4
 	github.com/swaggo/swag v1.16.4
+	google.golang.org/grpc v1.72.2
+	google.golang.org/protobuf v1.36.6
 )
 
 require (
@@ -19,8 +21,10 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/swaggo/files v0.0.0-20220610200504-28940afbdbfe // indirect
-	golang.org/x/net v0.23.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/tools v0.7.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/tools v0.21.1-0.20240508182429-e35e4ccd0d2d // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250218202821-56aae31c358a // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ internal/grpc/services/player_service.go
@@ -0,0 +1,68 @@
+package services
+
+import (
+	"context"
+
+	"github.com/moLIart/go-course/internal/grpc/generated"
+	"github.com/moLIart/go-course/internal/model/room"
+	"github.com/moLIart/go-course/internal/repository"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type PlayerService struct {
+	generated.UnimplementedPlayerServiceServer
+}
+
+func (s *PlayerService) GetPlayer(ctx context.Context, req *generated.RequestEntity) (*generated.GetPlayerDto, error) {
+	player := repository.GetPlayerByID(int(req.Id))
+	if player == nil {
+		return nil, status.Errorf(codes.NotFound, "player not found")
+	}
+	return &generated.GetPlayerDto{
+		Id:   int32(player.ID),
+		Name: player.Name,
+	}, nil
+}
+
+func (s *PlayerService) GetAllPlayers(ctx context.Context, _ *emptypb.Empty) (*generated.PlayerList, error) {
+	players := repository.GetPlayers()
+	playerDtos := make([]*generated.GetPlayerDto, len(players))
+	for i, player := range players {
+		playerDtos[i] = &generated.GetPlayerDto{
+			Id:   int32(player.ID),
+			Name: player.Name,
+		}
+	}
+	return &generated.PlayerList{Players: playerDtos}, nil
+}
+
+func (s *PlayerService) CreatePlayer(ctx context.Context, req *generated.CreatePlayerDto) (*generated.GetPlayerDto, error) {
+	player := room.NewPlayer(req.Name)
+	repository.AddEntity(player)
+	return &generated.GetPlayerDto{
+		Id:   int32(player.ID),
+		Name: player.Name,
+	}, nil
+}
+
+func (s *PlayerService) UpdatePlayer(ctx context.Context, req *generated.UpdatePlayerDto) (*generated.GetPlayerDto, error) {
+	ok := repository.UpdatePlayerByID(int(req.Id), req.Name)
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "player not found")
+	}
+	player := repository.GetPlayerByID(int(req.Id))
+	return &generated.GetPlayerDto{
+		Id:   int32(player.ID),
+		Name: player.Name,
+	}, nil
+}
+
+func (s *PlayerService) DeletePlayer(ctx context.Context, req *generated.RequestEntity) (*emptypb.Empty, error) {
+	ok := repository.DeletePlayerByID(int(req.Id))
+	if !ok {
+		return nil, status.Errorf(codes.NotFound, "player not found")
+	}
+	return &emptypb.Empty{}, nil
+}
